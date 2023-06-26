6ee89d0a1e67e4059baf61a8a7b90d5b84b24e8e
Konysheva Tatyana <tkonyseva693@gmail.com>
2023-06-27 00:56:45 +0500

    Added test

---
+++ src/test/java/ru/netology/qa/web/OrderCardTest.java
@@ -0,0 +1,55 @@
+package ru.netology.qa.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class OrderCardTest {
+    @Test
+    void shouldOrderCarsSuccess() {
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form_theme_alfa-on-white");
+        form.$("[data-test-id=name] input").setValue("Конышева Татьяна");
+        form.$("[data-test-id=phone] input").setValue("+79198886745");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button__text").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldOrderCarsErrorName() {
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form_theme_alfa-on-white");
+        form.$("[data-test-id=name] input").setValue("Tatyana Konyseva");
+        form.$("[data-test-id=phone] input").setValue("+79198886745");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button__text").click();
+        form.$(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldOrderCarsErrorPhone() {
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form_theme_alfa-on-white");
+        form.$("[data-test-id=name] input").setValue("Конышева Татьяна");
+        form.$("[data-test-id=phone] input").setValue("+111");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button__text").click();
+        SelenideElement invalidForm = $(".input_invalid");
+        invalidForm.$(".input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldOrderCarsErrorAgreement() {
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form_theme_alfa-on-white");
+        form.$("[data-test-id=name] input").setValue("Конышева Татьяна");
+        form.$("[data-test-id=phone] input").setValue("+65666665544");
+        form.$(".button__text").click();
+        SelenideElement invalidForm = $(".input_invalid");
+        invalidForm.$(".checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+}
